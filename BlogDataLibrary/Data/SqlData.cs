ceeec7cb254bd5a49b2316639dbb789c90cca2fd
waltvstheworld <walteriantejada@gmail.com>
2023-06-08 18:30:25 +0800

    LE2Tejada

---
+++ BlogDataLibrary/Data/SqlData.cs
@@ -0,0 +1,20 @@
+﻿using BlogDataLibrary.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDataLibrary.Data
+{
+    public class SqlData
+    {
+        private ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDb";
+
+        public SqlData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+    }
+}
